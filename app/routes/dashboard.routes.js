58af6ecde41574e1f192dd75a1401935f88733e0
CristiancitoUwU <cdramirez792@misena.edu.co>
2023-05-12 16:36:11 -0500

    FrontEnd

--- app/routes/dashboard.routes.js
+++ app/routes/dashboard.routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import fetch from 'node-fetch'
+import { response } from "express";
+
 dotenv.config();
 
 const dash = Router();
@@ -23,15 +26,32 @@ dash.get("/inicio", (req, res) => {
         res.redirect("/");
     }
 });
-
-dash.get("/Usuarios", (req, res) => {
+//Fetch donde se consume la Base de datos 
+dash.get("/Usuarios", async(req, res) => {
     if (req.cookies.ckswf){
     try {
         const token = jwt.verify(req.cookies.ckswf, process.env.SECRET_KEY);
+        let ruta = 'http://localhost:3000/apiUser/users';
+        let option = {
+            method:"GET"
+        }
+        let datos = {};
+        const respuesta = await fetch(ruta, option)
+        .then(response => response.json())
+        .then(data =>{
+            datos = data[0];
+            //console.log(data[0])
+        })
+        .catch(err => console.error("Error en la peticion" +err));
+
+
+
+
         res.render("dash", {
             "nombre":token.nombre,
             "foto":token.foto,
-            "menu": 1
+            "menu": 1,
+            "datos": datos
         });
 
     } catch (error) {
